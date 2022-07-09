167b9983bfc627c400c26664831c47e04ac8d69b
Dariusz Lichański <lichanskidariusz@gmail.com>
2022-07-09 13:09:59 +0200

    Adding main method in class Calculator

--- src/main/java/com/kalkulator/Calculator.java
+++ src/main/java/com/kalkulator/Calculator.java
@@ -9,4 +9,10 @@ public class Calculator {
     public int subtractAFromB(int a, int b) {
         return a - b;
     }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        int result = calculator.addAToB(45,5);
+        System.out.println("Wynik to: " + result);
+    }
 }
